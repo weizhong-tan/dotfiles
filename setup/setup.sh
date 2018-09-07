dc16928a5b8ce0a636010a2e7819b8694e162329
Wei-Zhong Tan <me@weizhongtan.com>
2018-09-07 20:34:59 +0100

    add setup file

---
+++ setup/setup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+source resources.sh
+
+bot "hello! welcome to your new computer"
+bot "let's get going! "
+
+bot "installing osx command line tools"
+xcode-select --install
+
+# set computer info
+yes_or_no "Would you like to set computer info now?"
+if confirmed; then
+  set_computer_info
+else
+  bot "ok, but remember to set it later (set_computer_info)"
+fi
+
+# setup git credentials
+yes_or_no "Would you like to set your git credentials now?"
+if confirmed; then
+  set_git_info
+else
+  bot "ok, but remember to do it before your first commit (set_git_info)!"
+fi
+
+# install brew
+running "installing brew"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+if [[ $? != 0 ]]; then
+    error "unable to install homebrew -> quitting setup"
+    exit 2
+fi
+
+running "updating to most recent brew version"
+brew doctor
+brew update
+ok
+
+running "Select which bundled brew & brew-cask packages you want to install"
+source installs/.brew_installs
+ok "feel free to add more brew packages! "
+
+# globally install key npm pkgs
+running "Select which bundled npm modules you want to install"
+source installs/.npm_installs
+ok "feel free to add more npm modules! "
+
+running "sourcing bashrc"
+source ~/.bashrc
+ok
+
+running "sourcing osx defaults"
+source .osx
+ok
+
+bot "whooo, all set! "
